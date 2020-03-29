07083cc9abdb20274b94c3865fad2302f766dd01
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-29 21:34:29 +0300

    done

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -12,13 +12,11 @@ export default class MasterHog extends Component {
     }
   }
 
-
   changeEyeColor = (e) => {
     this.setState({
       eyeColor: e.target.value
     })
   }
-
 
   render() {
     return (
@@ -40,9 +38,9 @@ export default class MasterHog extends Component {
         </div>
         
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {offspring.map(hog =>
+            <BabyHog name={hog.name} hobby={hog.hobby} eyeColor={this.state.eyeColor} />
+          )}
         </ul>
 
       </div>
